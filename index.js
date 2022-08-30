7b738143bec79d477b3b71a00bda3d48aed6a2fd
Faith Makinde <faithmakinde0@gmail.com>
2022-08-30 01:05:25 +0100

    work completed

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 let lengthEl = document.getElementById("length")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
+let temperatureEl = document.getElementById("temp-el")
+let speedEl = document.getElementById("speed-el")
 let first = null;
 let second = null
 let convertBtn = document.getElementById("convert-btn");
 convertBtn.addEventListener("click", function(){
     let inputEl = document.getElementById("input-el").value;
-    document.getElementById("input-el").value = ""
+    document.getElementById("input-el").value = "";
      first = inputEl * 3.281;
       second = inputEl / 3.281;
 
@@ -25,12 +27,23 @@ convertBtn.addEventListener("click", function(){
 
      massEl.innerText = `${inputEl} kilos = ${first.toFixed(3)} pounds | ${inputEl} pounds = ${second.toFixed(3)} kilos` ;
 
+     first =  (9/5 * inputEl) + 32;
+     second =  (inputEl - 32) * (5/9);
+
+     temperatureEl.innerText = `${inputEl} Celsuis = ${first.toFixed(3)} fahrenheit | ${inputEl} fahrenheit = ${second.toFixed(3)} celsuis`;
+    
+     first = inputEl / 1.609;
+     second = inputEl * 1.609;
+
+     speedEl.innerText = `${inputEl} km/hr = ${first.toFixed(3)} mph | ${inputEl} mph = ${second.toFixed(3)} km/hr`
+
      if(isNaN(inputEl)){
         let inputEl = document.getElementById("input-el").value;
         inputEl = "";
         lengthEl.innerText = `This is not a number, can't convert, Input a number`;
         volumeEl.innerText = `This is not a number, can't convert, Input a number`;
         massEl.innerText = `This is not a number, can't convert, Input a number` ;  
+        temperatureEl.innerText = `This is not a number, can't convert, Input a number`;
     }
   
 })
